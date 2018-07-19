1b819c785a213c307a593ac7f5cc4ea23dd4e20f
Cian <cianoleary98@gmail.com>
2018-07-19 12:50:05 +1000

    Tests Pass - Functional Complete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const JWT = require('jsonwebtoken')
+const Data = require('./data/events');
 
 const DecodeJWT = Token => new Promise((Resolve, Reject) => // The JWT Library does not support promises yet
   JWT.verify(Token, process.env.JWTSecret, function(err, payload) {
@@ -16,24 +17,35 @@ const DecodeJWT = Token => new Promise((Resolve, Reject) => // The JWT Library d
 
 const JWTMiddleware = (req, res, next) => {
   const AuthHeader = req.get('Authorization') || null;
-  const Token = AuthHeader.split(' ')[1] || "NoToken";
 
-  DecodeJWT(Token).then(Payload => {
-    req.User = Payload;
-    next();
-  }).catch(err => 
+  if (AuthHeader !== null) {
+    const Token = AuthHeader.split(' ')[1] || "NoToken";
+
+    DecodeJWT(Token).then(Payload => {
+      req.User = Payload;
+      next();
+    }).catch(err => 
+      res.status(403).json({
+        error: "Invalid Token"
+      })
+    );
+  } else {
     res.status(403).json({
-      error: "Invalid Token"
-    })
-  );
+      error: "No Token Provided"
+    });
+  }
 };
+
+const getFilterCities = (TargetCity, DataSet) => // Realistically this should be querying a Database of some sort
+  DataSet.filter(Event => Event.Location.City.toLowerCase() === TargetCity.toLowerCase());
 
 app.use(JWTMiddleware);
 
 app.get(['/events', '/events/:location'], (req, res) => {
-  res.json({
-    Hello: "World",
-  })
+  const Location = req.params.location || req.User.location;
+  const Cities = getFilterCities(Location, Data);
+  
+  res.json(Cities.slice(0, 5))
 });
 
 app.get('*', (req, res) => {
--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const app = require('./app')
 const PORT = 3001;
 const HOST = '0.0.0.0';
 
+process.env.JWTSecret = 'secret'; // This would be set by an .env file
+
+app.listen(PORT, HOST);
+
 console.log(`Running on http://${HOST}:${PORT}`);
